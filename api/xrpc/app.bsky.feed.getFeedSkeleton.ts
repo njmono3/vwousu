3cc490a1edb908e0e51537bb121acf8b350ca0bb
njmono3 <77790085+njmono3@users.noreply.github.com>
2024-12-07 22:27:14 +0900

    Update app.bsky.feed.getFeedSkeleton.ts

--- api/xrpc/app.bsky.feed.getFeedSkeleton.ts
+++ api/xrpc/app.bsky.feed.getFeedSkeleton.ts
@@ -7,7 +7,7 @@ export default function (req: NowRequest, res: NowResponse) {
                 "post": "at://did:plc:tpkejgkmpl7xz322emd5lwy2/app.bsky.feed.post/3lcev26u2ts25"
             },
             {
-                "post": "at://294aria.bsky.social/post/3lcck6d3dzc2w"
+                "post": "at://294aria.bsky.social/app.bsky.feed.post/3lcck6d3dzc2w"
             }
         ]
     });
