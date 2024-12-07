55f9eaa3dfe2bc1a7df0a8b595d55f1cdfd09576
njmono3 <77790085+njmono3@users.noreply.github.com>
2024-12-08 00:31:50 +0900

    Update app.bsky.feed.getFeedSkeleton.ts

--- api/xrpc/app.bsky.feed.getFeedSkeleton.ts
+++ api/xrpc/app.bsky.feed.getFeedSkeleton.ts
@@ -103,9 +103,10 @@ const sample_feed = `3lcpsnz5ewk2y2024-12-07 22:31:59
 
 export default function (req: NowRequest, res: NowResponse) {
     const limit = !Number.isNaN(req.limit*1) ? req.limit*1 : 50;
-    const cursor = !Number.isNaN((req.cursor + "").split("?")[0]*1) ? (req.cursor + "").split("?")[0]*1 : 0;
+    const cursor = !Number.isNaN(req.cursor*1) ? req.cursor*1 : 0;
     res.send({
-        ...(cursor + limit < sample_feed.length ? { "cursor": (cursor + limit) + "?" + JSON.stringify(req) } : {})
+        "req": req,
+        ...(cursor + limit < sample_feed.length ? { "cursor": cursor + limit } : {}),
         "feed": sample_feed.slice(cursor, Math.min(cursor + limit, sample_feed.length))
     });
 }
