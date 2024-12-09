4d474e2d5122fe800964c83c08dc9d84c49e6a73
njmono3 <2nmn003@gmail.com>
2024-12-09 18:14:31 +0900

    create xrpc file

--- api/xrpc/com.vwousu.report.postReport.ts
+++ api/xrpc/com.vwousu.report.postReport.ts
@@ -1,6 +1,6 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
 export default function GET(req: NowRequest, res: NowResponse) { 
-    res.json({ hoge: req.headers.get("Authorization"), fuga: process.env.BSKY_APP_PASSWORD });
+    res.json({ hoge: req.headers, fuga: process.env.BSKY_APP_PASSWORD });
     return;
 }
